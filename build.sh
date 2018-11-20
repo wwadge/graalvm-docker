a6cc8577345d5ae31a02f02c38684ea9c4ae9796
Wallace Wadge <wwadge@gmail.com>
2018-11-20 19:12:58 +0100

    Update to RC9 and download binary

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-GRAALVM_BUILD=${GRAALVM_BUILD:-"1.0.0-rc4"}
+GRAALVM_BUILD=${GRAALVM_BUILD:-"1.0.0-rc9"}
 
-docker build . --build-arg GRAALVM_BUILD=$GRAALVM_BUILD --tag panga/graalvm-ce:$GRAALVM_BUILD --tag panga/graalvm-ce:latest
+FILENAME=graalvm-ce-$GRAALVM_BUILD-linux-amd64.tar.gz
+URL=https://github.com/oracle/graal/releases/download/vm-$GRAALVM_BUILD/$FILENAME
+
+
+if ! [ -f $FILENAME ]; then
+curl -L -o $FILENAME $URL
+fi
+
+docker build . --build-arg GRAALVM_BUILD=$GRAALVM_BUILD --tag wwadge/graalvm-ce:$GRAALVM_BUILD --tag wwadge/graalvm-ce:latest
